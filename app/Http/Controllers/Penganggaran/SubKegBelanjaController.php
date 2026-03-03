f0e71bb8213528fe129d5c15d1e4b2880c255ff3
Rizki Anggri Putra <ikyanggriputra001@gmail.com>
2026-03-04 07:55:55 +0900

    jadwal

---
+++ app/Http/Controllers/Penganggaran/SubKegBelanjaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Penganggaran;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubKegBelanjaController extends Controller
+{
+    //
+}
